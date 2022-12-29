e1978e3dd42b08665ec2e026a27087b45941eefa
JerryLiang317 <a0988768311@gmail.com>
2022-12-30 01:53:14 +0800

    商品功能 - 實作查詢商品列表 (基礎實作)

--- src/main/java/com/liangjerry/springmall/dao/ProductDao.java
+++ src/main/java/com/liangjerry/springmall/dao/ProductDao.java
@@ -3,7 +3,11 @@ package com.liangjerry.springmall.dao;
 import com.liangjerry.springmall.dto.ProductRequest;
 import com.liangjerry.springmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+
+    List<Product> getProducts();
 
     Product getProductById (Integer productId);
 
--- src/main/java/com/liangjerry/springmall/service/ProductService.java
+++ src/main/java/com/liangjerry/springmall/service/ProductService.java
@@ -3,7 +3,11 @@ package com.liangjerry.springmall.service;
 import com.liangjerry.springmall.dto.ProductRequest;
 import com.liangjerry.springmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getProducts();
 
     Product getProductById (Integer productId);
 
@@ -12,4 +16,5 @@ public interface ProductService {
     void updateProduct(Integer productId, ProductRequest productRequest);
 
     void deleteProductById(Integer productId);
+
 }
